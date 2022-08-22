24868f08567b406ddb5b0f0d7d38a1fc458d11bf
Dudenin <109083526+Dudenin@users.noreply.github.com>
2022-08-22 23:51:52 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,7 @@
+a = "hellow"
+b = "world!"
+print(f"{a}, {b}")
+numb_1 = int(input("Enter number:"))
+numb_2 = int(input("Enter number second time:"))
+word_1 = input("Enter any word:")
+print(f"You have chosen numbers {numb_1} and {numb_2} and word {word_1}")
